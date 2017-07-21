b7b0b35354e8f91c9a5f48ec4ba420914d0287de
theapache64-charlie <theapache64@gmail.com>
2017-07-21 16:54:09 +0530

    First commit

---
+++ app/src/main/java/com/avenues/lib/testotpappnew/InitialActivity.java
@@ -0,0 +1,66 @@
+package com.avenues.lib.testotpappnew;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import Utility.AvenuesParams;
+import Utility.ServiceUtility;
+
+
+public class InitialActivity extends ActionBarActivity {
+
+    private EditText accessCode, merchantId, currency, amount, orderId, rsaKeyUrl, redirectUrl, cancelUrl;
+    private void init(){
+        accessCode = (EditText) findViewById(R.id.accessCode);
+        merchantId = (EditText) findViewById(R.id.merchantId);
+        orderId  = (EditText) findViewById(R.id.orderId);
+        currency = (EditText) findViewById(R.id.currency);
+        amount = (EditText) findViewById(R.id.amount);
+        rsaKeyUrl = (EditText) findViewById(R.id.rsaUrl);
+        redirectUrl = (EditText) findViewById(R.id.redirectUrl);
+        cancelUrl = (EditText) findViewById(R.id.cancelUrl);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+        init();
+        //generating order number
+        Integer randomNum = ServiceUtility.randInt(0, 9999999);
+        orderId.setText(randomNum.toString());
+    }
+
+    public void onClick(View view) {
+        String vAccessCode = ServiceUtility.chkNull(accessCode.getText()).toString().trim();
+        String vMerchantId = ServiceUtility.chkNull(merchantId.getText()).toString().trim();
+        String vCurrency = ServiceUtility.chkNull(currency.getText()).toString().trim();
+        String vAmount = ServiceUtility.chkNull(amount.getText()).toString().trim();
+        if(!vAccessCode.equals("") && !vMerchantId.equals("") && !vCurrency.equals("") && !vAmount.equals("")){
+            Intent intent = new Intent(this,WebViewActivity.class);
+            intent.putExtra(AvenuesParams.ACCESS_CODE, ServiceUtility.chkNull(accessCode.getText()).toString().trim());
+            intent.putExtra(AvenuesParams.MERCHANT_ID, ServiceUtility.chkNull(merchantId.getText()).toString().trim());
+            intent.putExtra(AvenuesParams.ORDER_ID, ServiceUtility.chkNull(orderId.getText()).toString().trim());
+            intent.putExtra(AvenuesParams.CURRENCY, ServiceUtility.chkNull(currency.getText()).toString().trim());
+            intent.putExtra(AvenuesParams.AMOUNT, ServiceUtility.chkNull(amount.getText()).toString().trim());
+
+            intent.putExtra(AvenuesParams.REDIRECT_URL, ServiceUtility.chkNull(redirectUrl.getText()).toString().trim());
+            intent.putExtra(AvenuesParams.CANCEL_URL, ServiceUtility.chkNull(cancelUrl.getText()).toString().trim());
+            intent.putExtra(AvenuesParams.RSA_KEY_URL, ServiceUtility.chkNull(rsaKeyUrl.getText()).toString().trim());
+
+            startActivity(intent);
+        }else{
+            showToast("All parameters are mandatory.");
+        }
+    }
+
+    public void showToast(String msg) {
+        Toast.makeText(this, "Toast: " + msg, Toast.LENGTH_LONG).show();
+    }
+}
